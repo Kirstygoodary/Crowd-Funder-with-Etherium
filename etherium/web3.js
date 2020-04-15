1d7fa0745f1304c3625f25eb52fe5fda2326efb9
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-04-15 18:21:55 +0100

    code to ensure users without MM can use the app

--- etherium/web3.js
+++ etherium/web3.js
@@ -1,6 +1,26 @@
-import web3 from "web3";
+import Web3 from "web3";
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+/**
+ * Not all browsers have metamask running so the 'window' variable may be undefined.
+ */
+
+if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
+  // if true then we are in the browser and metamask is running :)
+  web3 = new Web3(window.web3.currentProvider);
+} else {
+  const provider = new Web3.providers.HttpProvider(
+    "https://rinkeby.infura.io/v3/067ff9bc69b648509759a2e25e9f305d"
+  ); // link is from deploy.js
+  // infura provides another way to access the Rinkeby network.
+
+  web3 = new Web3(provider);
+
+  //We are on the server OR the user is not running metamask
+  // Therefore we are creating a new provider
+}
+
+//const web3 = new Web3(window.web3.currentProvider);
 /**
  * Creating a new instance of web3 and using the provider
  * from Metamask.
